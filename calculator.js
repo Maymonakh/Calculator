ecc4d48b2b7652fe135feaa2855147475f979684
Maymonakh <maymonakhanfar@gmail.com>
2023-09-15 00:11:40 +0300

    Handle ignoring numbers greater than 1000

--- calculator.js
+++ calculator.js
@@ -1,14 +1,14 @@
 function calc(...args) {
   if (args.length % 2 === 0) {
-    throw new Error('Invalid input');
+    throw new Error("Invalid input");
   }
 
-  const operators = ['*', '/', '+', '-'];
+  const operators = ["*", "/", "+", "-"];
   const precedence = {
-    '*': 2,
-    '/': 2,
-    '+': 1,
-    '-': 1,
+    "*": 2,
+    "/": 2,
+    "+": 1,
+    "-": 1,
   };
 
   const evaluate = (values, ops) => {
@@ -17,23 +17,23 @@ function calc(...args) {
       const b = values.pop();
       const a = values.pop();
       switch (op) {
-        case '+':
+        case "+":
           values.push(a + b);
           break;
-        case '-':
+        case "-":
           values.push(a - b);
           break;
-        case '*':
+        case "*":
           values.push(a * b);
           break;
-        case '/':
+        case "/":
           if (b === 0) {
-            throw new Error('Division by zero');
+            throw new Error("Division by zero");
           }
           values.push(a / b);
           break;
         default:
-          throw new Error('Invalid operator');
+          throw new Error("Invalid operator");
       }
     }
   };
@@ -44,13 +44,18 @@ function calc(...args) {
   for (let i = 0; i < args.length; i++) {
     const token = args[i];
     if (i % 2 === 0) {
-      if (typeof token !== 'number') {
-        throw new Error('Invalid input type');
+      if (typeof token !== "number") {
+        throw new Error("Invalid input type");
       }
-      values.push(token);
+      // Check if the number is greater than 1000 and skip it
+      if (token > 1000) {
+        values.push(0); 
+      } else {
+        values.push(token); 
+      }
     } else {
       if (!operators.includes(token)) {
-        throw new Error('Invalid operator');
+        throw new Error("Invalid operator");
       }
       while (
         ops.length > 0 &&
@@ -65,7 +70,7 @@ function calc(...args) {
   evaluate(values, ops);
 
   if (values.length !== 1) {
-    throw new Error('Invalid input');
+    throw new Error("Invalid input");
   }
 
   return values[0];
--- calculator.test.js
+++ calculator.test.js
@@ -1,67 +1,73 @@
 // calculator.test.js
-const calc = require('./calculator');
+const calc = require("./calculator");
 
-describe('Calculator', () => {
+describe("Calculator", () => {
   // Test case: Addition
-  it('should return the correct sum of two numbers', () => {
-    expect(calc(2, '+', 3)).toBe(5);
+  it("should return the correct sum of two numbers", () => {
+    expect(calc(2, "+", 3)).toBe(5);
   });
 
   // Test case: Subtraction
-  it('should return the correct difference of two numbers', () => {
-    expect(calc(5, '-', 2)).toBe(3);
+  it("should return the correct difference of two numbers", () => {
+    expect(calc(5, "-", 2)).toBe(3);
   });
 
   // Test case: Multiplication
-  it('should return the correct product of two numbers', () => {
-    expect(calc(4, '*', 6)).toBe(24);
+  it("should return the correct product of two numbers", () => {
+    expect(calc(4, "*", 6)).toBe(24);
   });
 
   // Test case: Division
-  it('should return the correct quotient of two numbers', () => {
-    expect(calc(10, '/', 2)).toBe(5);
+  it("should return the correct quotient of two numbers", () => {
+    expect(calc(10, "/", 2)).toBe(5);
   });
 
   // Test case: Division by zero
-  it('should throw an error when dividing by zero', () => {
-    expect(() => calc(6, '/', 0)).toThrow('Division by zero');
+  it("should throw an error when dividing by zero", () => {
+    expect(() => calc(6, "/", 0)).toThrow("Division by zero");
   });
 
   // Test case: Negative numbers
-  it('should handle negative numbers correctly', () => {
-    expect(calc(-8, '+', 5)).toBe(-3);
+  it("should handle negative numbers correctly", () => {
+    expect(calc(-8, "+", 5)).toBe(-3);
   });
 
   // Test case: Decimal numbers
-  it('should handle decimal numbers correctly', () => {
-    expect(calc(3.5, '*', 2)).toBe(7);
+  it("should handle decimal numbers correctly", () => {
+    expect(calc(3.5, "*", 2)).toBe(7);
   });
 
   // Test case: Order of operations
-  it('should follow the correct order of operations', () => {
-    expect(calc(2, '+', 3, '*', 4)).toBe(14);
-    expect(calc(2, '*', 3, '+', 4)).toBe(10);
-    expect(calc(10, '-', 5, '/', 2)).toBe(7.5);
+  it("should follow the correct order of operations", () => {
+    expect(calc(2, "+", 3, "*", 4)).toBe(14);
+    expect(calc(2, "*", 3, "+", 4)).toBe(10);
+    expect(calc(10, "-", 5, "/", 2)).toBe(7.5);
   });
 
   // Test case: Invalid operator
-  it('should throw an error for an invalid operator', () => {
-    expect(() => calc(5, '$', 3)).toThrow('Invalid operator');
+  it("should throw an error for an invalid operator", () => {
+    expect(() => calc(5, "$", 3)).toThrow("Invalid operator");
   });
 
   // Test case: Invalid input type
-  it('should throw an error for invalid input types', () => {
-    expect(() => calc('2', '+', 3)).toThrow('Invalid input type');
-    expect(() => calc(2, '+', '3')).toThrow('Invalid input type');
+  it("should throw an error for invalid input types", () => {
+    expect(() => calc("2", "+", 3)).toThrow("Invalid input type");
+    expect(() => calc(2, "+", "3")).toThrow("Invalid input type");
   });
 
   // Test multiple operations in a single expression.
-  test('Multiple Operations', () => {
-    expect(calc(2, '+', 3, '+', 4)).toBe(9);
-    expect(calc(2, '*', 3, '*', 4)).toBe(24);
-    expect(calc(10, '-', 5, '-', 2)).toBe(3);
-    expect(calc(8, '*', 2, '+', 5, '/', 5)).toBe(17);
-    expect(calc(10, '/', 2, '*', 3)).toBe(15);
-    expect(calc(2, '-', 3, '+', 4, '*', 2)).toBe(7);
+  test("Multiple Operations", () => {
+    expect(calc(2, "+", 3, "+", 4)).toBe(9);
+    expect(calc(2, "*", 3, "*", 4)).toBe(24);
+    expect(calc(10, "-", 5, "-", 2)).toBe(3);
+    expect(calc(8, "*", 2, "+", 5, "/", 5)).toBe(17);
+    expect(calc(10, "/", 2, "*", 3)).toBe(15);
+    expect(calc(2, "-", 3, "+", 4, "*", 2)).toBe(7);
+  });
+
+  // Numbers greater than 1000 should be ignored.
+  test("Ignore Numbers Greater Than 1000", () => {
+    expect(calc(2, "+", 1001)).toBe(2);
+    expect(calc(1001, "-", 5)).toBe(-5);
   });
 });
